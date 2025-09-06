73b77100b064a17311e2f1866d6a46528a00efdb
chris Taylor <ctay456a@gmail.com>
2025-09-06 18:24:56 -0400

    Create login.component.ts

---
+++ frontend/src/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email = '';
+  password = '';
+  errorMessage = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit() {
+    this.authService.login({ email: this.email, password: this.password }).subscribe({
+      next: () => this.router.navigate(['/dashboard']),
+      error: () => this.errorMessage = 'Invalid email or password'
+    });
+  }
+}
