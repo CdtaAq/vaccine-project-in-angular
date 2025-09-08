4f85ebf0f4d185bd593e1c711a5a60319517cb02
chris Taylor <ctay456a@gmail.com>
2025-09-07 20:23:54 -0400

    Update Hospital.java

--- backend/src/main/java/com/vaccination/model/Hospital.java
+++ backend/src/main/java/com/vaccination/model/Hospital.java
@@ -1,12 +1,58 @@
 package com.vaccination.model;
+
 import jakarta.persistence.*;
+
 @Entity
-@Table(name="hospitals")
+@Table(name = "hospitals")
 public class Hospital {
-  @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
-  private String name;
-  private String address;
-  private String type;
-  private Double charges;
-  // getters/setters
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String address;
+    private String type;
+    private Double charges;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getCharges() {
+        return charges;
+    }
+
+    public void setCharges(Double charges) {
+        this.charges = charges;
+    }
 }
